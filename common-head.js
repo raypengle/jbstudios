a003e94d670656962ee315c9b8c1289caa5d28e9
raypengle <2367358808@qq.com>
2026-07-16 11:11:54 +0800

    修改了logo文件和header逻辑

---
+++ common-head.js
@@ -0,0 +1,7 @@
+(function () {
+  var title = window.pageTitle || '欢乐豆工作室';
+  document.write('<meta charset="UTF-8">');
+  document.write('<title>' + title + '</title>');
+  document.write('<link rel="stylesheet" type="text/css" href="style.css">');
+  document.write('<!-- <script src="script.js"></script> -->');
+})();
