e0208d6117087f57ed80bf0ad8ee99e7b40dfe8b
raypengle <2367358808@qq.com>
2026-07-21 19:42:48 +0800

    优化了移动端体验

--- common-head.js
+++ common-head.js
@@ -1,6 +1,7 @@
 (function () {
   var title = window.pageTitle || '欢乐豆工作室';
   document.write('<meta charset="UTF-8">');
+  document.write('<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">');
   document.write('<title>' + title + '</title>');
   document.write('<link rel="icon" type="image/png" href="./images/icon_web.png">');
   document.write('<link rel="stylesheet" type="text/css" href="style.css">');
